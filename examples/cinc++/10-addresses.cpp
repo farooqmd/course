033cee973dbee250a629390a0028b9a459fb2da7
Olaf Lenz <olaf@lenz.name>
2013-02-19 06:52:29 +0100

    Fininshed

--- examples/cinc++/10-addresses.cpp
+++ examples/cinc++/10-addresses.cpp
@@ -6,25 +6,44 @@
 using namespace std;
 
 // * look at the addresses of variables
-// * type casting
-int a, b, c, d;
 
-void f() {
-  int fi;
-  int fj;
-  cout << "fi: " << (long)&fi << endl;
-  cout << "fj: " << (long)&fj << endl;
+// * global variables
+//   * will exist during the whole program 
+//   * in own memory region
+int global_variable1;
+int global_variable2;
+int global_variable3;
+int global_variable4;
+
+// * local variables and function parameters
+//   * generated when function is called
+//   * destroyed when function ends
+//   * created on the STACK
+void function1(int function_parameter1, int function_parameter2) {
+  int local_variable1;
+  int local_variable2;
+  cout << "local_variable1: " << (long)&local_variable1 << endl;
+  cout << "local_variable2: " << (long)&local_variable2 << endl;
+  cout << "function_parameter1: " << (long)&function_parameter1 << endl;
+  cout << "function_parameter2: " << (long)&function_parameter2 << endl;
 }
 
+void function2() {}
+
 int main() {
-  int i, j, k;
-  cout << "f(): " << (long)&f << endl;
-  cout << "a: " << (long)&a << endl;
-  cout << "b: " << (long)&b << endl;
-  cout << "c: " << (long)&c << endl;
-  cout << "d: " << (long)&d << endl;
-  cout << "i: " << (long)&i << endl;
-  cout << "j: " << (long)&j << endl;
-  cout << "k: " << (long)&k << endl;
-  f();
+  // * main variables: top of the stack
+  int main_variable1;
+  int main_variable2;
+
+  // * function itself: own memory region
+  cout << "function1(): " << (long)&function1 << endl;
+  cout << "function2(): " << (long)&function2 << endl;
+  cout << "global_variable1: " << (long)&global_variable1 << endl;
+  cout << "global_variable2: " << (long)&global_variable2 << endl;
+  cout << "global_variable3: " << (long)&global_variable3 << endl;
+  cout << "global_variable4: " << (long)&global_variable4 << endl;
+  cout << "main_variable1: " << (long)&main_variable1 << endl;
+  cout << "main_variable2: " << (long)&main_variable2 << endl;
+
+  function1(main_variable1, main_variable2);
 }
